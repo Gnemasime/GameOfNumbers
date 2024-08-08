cdf6dd98740a0228bcd891db6ca0c9482b78ce05
22123521 <22123521@ITWINLAB-8>
2024-08-08 21:46:39 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace GameOfNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblResults = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(362, 337);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(622, 337);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(145, 50);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(443, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(443, 129);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(135, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(443, 84);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(135, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(273, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter the first number:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(273, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Enter the second number:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblResult);
+            this.groupBox1.Controls.Add(this.lblResults);
+            this.groupBox1.Location = new System.Drawing.Point(162, 191);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(560, 124);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(273, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Enter the code:";
+            // 
+            // lblResults
+            // 
+            this.lblResults.AutoSize = true;
+            this.lblResults.Location = new System.Drawing.Point(7, 16);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(0, 13);
+            this.lblResults.TabIndex = 10;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(101, 94);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Game Of Numbers";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblResults;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameOfNumbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             try
+                {
+                    int firstNumber = int.Parse(textBox1.Text);
+                    int secondNumber = int.Parse(textBox3.Text);
+                    int operation = int.Parse(textBox2.Text);
+
+                    switch (operation)
+                    {
+                        case 1:
+                        lblResults.Text = $"Enter the calculation code: \n" +
+                        $"1 to get the sum of the two numbers \n" +
+                        $"2 to get the difference between the two numbers \n" +
+                        $"3 to get the products of the two numbers \n" +
+                        $"4 to get the quotient of the two numbers \n" +
+                        $"**************************************** \n" +
+                        $"  The result is =  {firstNumber + secondNumber}";
+                        break;
+                        case 2:
+                        lblResults.Text = $"Enter the calculation code: \n" +
+                         $"1 to get the sum of the two numbers \n" +
+                         $"2 to get the difference between the two numbers \n" +
+                         $"3 to get the products of the two numbers \n" +
+                         $"4 to get the quotient of the two numbers \n" +
+                         $"**************************************** \n" +
+                         $"  The result is =  {firstNumber - secondNumber}";
+                        break;
+                    case 3:
+                        lblResults.Text = $"Enter the calculation code: \n" +
+                         $"1 to get the sum of the two numbers \n" +
+                         $"2 to get the difference between the two numbers \n" +
+                         $"3 to get the products of the two numbers \n" +
+                         $"4 to get the quotient of the two numbers \n" +
+                         $"**************************************** \n" +
+                         $"  The result is =  {firstNumber * secondNumber}";
+                        break;
+                        case 4:
+                            if (secondNumber != 0)
+                            lblResults.Text = $"Enter the calculation code: \n" +
+                         $"1 to get the sum of the two numbers \n" +
+                         $"2 to get the difference between the two numbers \n" +
+                         $"3 to get the products of the two numbers \n" +
+                         $"4 to get the quotient of the two numbers \n" +
+                         $"**************************************** \n" +
+                         $"  The result is =  {firstNumber / (double)secondNumber}";
+                        else
+                                lblResults.Text = "Error: Division by zero!";
+                            break;
+                        default:
+                           lblResults.Text = "Error: Invalid operation code!";
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    lblResults.Text = $"Error: {ex.Message}";
+                }
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                lblResult.Text = string.Empty;
+            
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
